81ea9ca87cb57da687961467849af48c0f55872b
Nikhil Singh <nikhilsingh498@gmail.com>
2021-03-01 14:36:52 +0530

    CORS fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://risewithpragati.space');
+    res.header('Access-Control-Allow-Origin', 'https://www.risewithpragati.space');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     next();
 });
